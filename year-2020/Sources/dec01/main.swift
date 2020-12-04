365271e202792368afd6380af3c52a708ce4be81
Kalissaac <kalissaac@protonmail.com>
2020-12-04 11:54:55 -0800

    Move input to bottom of file

--- year-2020/Sources/dec01/main.swift
+++ year-2020/Sources/dec01/main.swift
@@ -1,5 +1,57 @@
 import Foundation
 
+let inputArrayRaw = input.split(separator: "\n")
+var inputArray = Array.init(repeating: 0, count: inputArrayRaw.count)
+
+for (i, v) in inputArrayRaw.enumerated() {
+    inputArray[i] = Int(v)!
+}
+
+func partOne(array: [Int]) -> Int {
+    var numA: Int = 0
+    var numB: Int = 0
+    for i in array {
+        for j in array {
+            if i + j == 2020 {
+                numA = i
+                numB = j
+                break
+            }
+        }
+        if numA != 0 && numB != 0 {
+            break
+        }
+    }
+    return numA * numB
+}
+
+func partTwo(array: [Int]) -> Int {
+    var numA: Int = 0
+    var numB: Int = 0
+    var numC: Int = 0
+    for i in array {
+        for j in array {
+            for k in array {
+                if i + j + k == 2020 {
+                    numA = i
+                    numB = j
+                    numC = k
+                    break
+                }
+            }
+        }
+        if numA != 0 && numB != 0 && numC != 0 {
+            break
+        }
+    }
+    return numA * numB * numC
+}
+
+print("Part One: \(partOne(array: inputArray))")
+print("Part Two: \(partTwo(array: inputArray))")
+
+// MARK: - Input
+
 let input = """
 408
 1614
@@ -202,53 +254,3 @@ let input = """
 1442
 1224
 """
-
-let inputArrayRaw = input.split(separator: "\n")
-var inputArray = Array.init(repeating: 0, count: inputArrayRaw.count)
-
-for (i, v) in inputArrayRaw.enumerated() {
-    inputArray[i] = Int(v)!
-}
-
-func partOne(array: [Int]) -> Int {
-    var numA: Int = 0
-    var numB: Int = 0
-    for i in array {
-        for j in array {
-            if i + j == 2020 {
-                numA = i
-                numB = j
-                break
-            }
-        }
-        if numA != 0 && numB != 0 {
-            break
-        }
-    }
-    return numA * numB
-}
-
-func partTwo(array: [Int]) -> Int {
-    var numA: Int = 0
-    var numB: Int = 0
-    var numC: Int = 0
-    for i in array {
-        for j in array {
-            for k in array {
-                if i + j + k == 2020 {
-                    numA = i
-                    numB = j
-                    numC = k
-                    break
-                }
-            }
-        }
-        if numA != 0 && numB != 0 && numC != 0 {
-            break
-        }
-    }
-    return numA * numB * numC
-}
-
-print("Part One: \(partOne(array: inputArray))")
-print("Part Two: \(partTwo(array: inputArray))")
