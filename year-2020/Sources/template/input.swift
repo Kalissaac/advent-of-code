97eca971a0bc2600e539bceb44c1dc4649924176
Kalissaac <kalissaac@protonmail.com>
2020-12-06 10:07:36 -0800

    Add day template

---
+++ year-2020/Sources/template/input.swift
@@ -0,0 +1,20 @@
+//
+//  input.swift
+//  
+//  Puzzle input
+//  Advent of Code 2020
+//
+
+import Foundation
+
+public class Input {
+    
+    public let dayNumberReference = """
+
+    """.components(separatedBy: "\n")
+    
+    public let dayNumber = """
+
+    """.components(separatedBy: "\n")
+    
+}
---
+++ year-2020/Sources/template/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  
+//  Puzzle solution
+//  Advent of Code 2020
+//
+
+import Foundation
+
+func partOne() -> Int {
+    return 0
+}
+
+func partTwo() -> Int {
+    return 0
+}
+
+print("\(partOne()) part one solution")
+print("\(partTwo()) part two solution")
